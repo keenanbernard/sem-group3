4c5e795b340f20fa354a8aa8bd13ac1c2b789e48
roberto-marin <40524486@live.napier.ac.uk>
2022-03-23 20:50:42 -0600

    SEM-11 Created Top N Countries by Region Report

--- src/main/java/com/napier/sem/ReportingApp.java
+++ src/main/java/com/napier/sem/ReportingApp.java
@@ -17,7 +17,9 @@ public class ReportingApp {
         //c.countries();
         // cy.allCities();
         // cy.citiesByRegion();
-        cy.TopNCities();
+        //cy.TopNCities();
+
+        c.TopNCountriesbyRegion();
 
         /*  // Get Country
         ArrayList<Country> countries = a.getCountry();
