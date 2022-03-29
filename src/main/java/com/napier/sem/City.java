46718ad264fded5af5b95fb55451d676522a2d8b
Jessica <98279129+jessica-magana@users.noreply.github.com>
2022-03-29 11:16:21 -0600

    	src/main/java/com/napier/sem/City.java

--- src/main/java/com/napier/sem/City.java
+++ src/main/java/com/napier/sem/City.java
@@ -59,6 +59,14 @@ public class City {
         printCityReport(topNCities);
     }
 
+    public void TopNCitiesbyCountry(){
+        ArrayList<City> topNCities = getTopNCitiesbyCountry(1);
+
+        System.out.println(topNCities.size());
+
+        printCityReport(topNCities);
+    }
+
     public ArrayList<City> getCities() {
         try {
             Connection con = ra.connect();
@@ -158,6 +166,7 @@ public class City {
         }
     }
 
+
     public ArrayList<City> getTopNCities(int rank) {
         try {
             Connection con = ra.connect();
@@ -254,6 +263,37 @@ public class City {
         }
     }
 
+    public ArrayList<City> getTopNCitiesbyCountry(int rank) {
+        try {
+            Connection con = ra.connect();
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT * from (SELECT cy.name, cy.countrycode, cy.district, cy.population, row_number() over (partition by cy.country order by cy.population desc) as cityRank "
+                            + "FROM city cy) ranks "
+                            + "WHERE cityRank <= " + rank;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            ArrayList<City> tpNCities = new ArrayList<>();
+            while (rset.next()) {
+                City cty = new City();
+                cty.name = rset.getString("name");
+                cty.countrycode = rset.getString("countrycode");
+                cty.district = rset.getString("district");
+                cty.population = rset.getInt("population");
+                tpNCities.add(cty);
+            }
+            return tpNCities;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get table details");
+            return null;
+        }
+    }
     public void printCityReport(ArrayList<City> cities) {
         System.out.println(String.format("%-10s %-15s %-15s %-20s", "name", "countrycode", "district", "population"));
 
