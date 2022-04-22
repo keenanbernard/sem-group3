efb87405677efd630faea7cd316a6660173ffb02
marlonbze <98763314+marlonbze@users.noreply.github.com>
2022-04-22 14:33:11 -0600

    SEM-13 Made edits to CitybyContinent report

--- src/main/java/com/napier/sem/City.java
+++ src/main/java/com/napier/sem/City.java
@@ -157,9 +157,9 @@ public class City {
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    " SELECT c.name, c.continent, c.population as country cy.countrycode, cy.district "
-                            + "FROM city cy, country c "
-                            + "order by c.population desc";
+                    "SELECT cy.name, c.continent, cy.population \n" +
+                            "FROM city cy, country c \n" +
+                            "order by c.population desc";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
