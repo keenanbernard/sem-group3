a1a3480b244edb464837e08ecaed84e784987e0c
marlonbze <98763314+marlonbze@users.noreply.github.com>
2022-04-22 13:59:50 -0600

    SEM-35 TopN Population of a District, Made edits and revised it

--- src/main/java/com/napier/sem/Population.java
+++ src/main/java/com/napier/sem/Population.java
@@ -263,8 +263,8 @@ public class Population {
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT * from (SELECT c.name, c.countrycode, cy.district, c.population, row_number() over (partition by pn.region order by pn.population desc) as countryRank "
-                            + "FROM city cy, population pn where cy.countrycode = c.code) ranks "
+                    "SELECT * from (SELECT c.name, c.countrycode, cy.district, c.population, row_number() over (partition by c.region order by c.population desc) as countryRank "
+                            + "FROM city cy, country c where cy.countrycode = c.code) ranks "
                             + "WHERE countryRank <= " + rank;
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
@@ -295,9 +295,9 @@ public class Population {
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT * from (SELECT cy.name, cy.countrycode, cy.district, cy.population, row_number() over (partition by cy.district order by cy.population desc) as cityRank "
-                            + "FROM city cy) ranks "
-                            + "WHERE cityRank <= " + rank;
+                    "SELECT * from (SELECT c.name, cy.countrycode ,cy.district, c.population, row_number() over (partition by c.region order by c.population desc) as countryRank \n" +
+                            "FROM city cy, country c where cy.countrycode = c.code) ranks\n" +
+                            "WHERE countryRank <= " + rank;
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
@@ -307,8 +307,7 @@ public class Population {
                 Population pn = new Population();
                 pn.name = rset.getString("name");
                 pn.population = rset.getBigDecimal("population");
-                pn.urban = rset.getString("urban");
-                pn.rural = rset.getString("rural");
+                pn.district = rset.getString("district");
                 tpNDistrict.add(pn);
             }
             return tpNDistrict;
