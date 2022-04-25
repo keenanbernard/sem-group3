64d2bc3c02bf35f58f058a9abaf3c4c8a7a1d81a
Jessica <98279129+jessica-magana@users.noreply.github.com>
2022-04-24 19:03:31 -0600

    SEM-40 Capital code review.

--- src/main/java/com/napier/sem/Capital.java
+++ src/main/java/com/napier/sem/Capital.java
@@ -67,6 +67,7 @@ public class Capital {
             // Create an SQL statement
             Statement stmt = con.createStatement();
             // Create string for SQL statement
+            //
             String strSelect =
                     "SELECT cy.name, c.name as country, cy.population "
                             + "FROM city cy, country c WHERE cy.id = c.capital "
